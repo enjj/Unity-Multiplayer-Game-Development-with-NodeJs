40ac32d394b2b75ac41e7791a13e75e42e185ccf
enjj <burakulusahan96@gmail.com>
2019-11-24 00:53:24 +0300

    Navigate Player

---
+++ UnityNode/Assets/Scripts/ClickMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMove : MonoBehaviour {
+
+    public GameObject player;
+
+
+    // Update is called once per frame
+    public void OnClick(Vector3 pos) {
+        NavigatePosition navPos = player.GetComponent<NavigatePosition>();
+        navPos.navigateTo(pos);
+    }
+}
---
+++ UnityNode/Assets/Scripts/ScreenClicker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenClicker : MonoBehaviour {
+
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire2"))
+            Clicked();
+
+    }
+
+    private void Clicked() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit)) {
+            Debug.Log(hit.collider.gameObject.name);
+
+            ClickMove clickMove = hit.collider.gameObject.GetComponent<ClickMove>();
+            clickMove.OnClick(hit.point);
+        }
+    }
+}
