43e389c6d5046a7ba41479b776060739bdfc5c6c
enjj <burakulusahan96@gmail.com>
2019-11-24 00:10:30 +0300

    Add player spawn

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,29 @@ const io = require('socket.io')(process.env.PORT || 3000);
 
 console.log('server started');
 
+let playerCount = 0;
+
 io.on('connection', (socket) =>{
   console.log('client Connected');
-  
-})
+
+  socket.broadcast.emit('spawn');
+  playerCount++;
+
+  for(i=0; i < playerCount; i++){
+
+    socket.emit('spawn');
+    console.log('sending spawn to new player');
+
+  }
+
+  socket.on('move', (data) =>{
+    console.log('client moved');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('client Disconnected');
+    playerCount--;
+
+  })
+
+}) 
--- UnityNode/Assets/Network.cs
+++ UnityNode/Assets/Network.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using SocketIO;
+using System;
 
-public class Network : MonoBehaviour
-{
+public class Network : MonoBehaviour {
     static SocketIOComponent socket;
 
-    void Start()
-    {
+    public GameObject playerPrefab;
+
+    void Start() {
         socket = GetComponent<SocketIOComponent>();
-        socket.On("open", OnConnected);  //neden parantez yok ?
+        socket.On("open", OnConnected);  //neden parantez yok ? 
+        socket.On("spawn", OnSpawned);
+
+    }
+
+    private void OnSpawned(SocketIOEvent e) {
+        Instantiate(playerPrefab);
     }
 
     void OnConnected(SocketIOEvent e) {
         Debug.Log("Connected");
-        socket.Emit("test");
+        socket.Emit("move");
     }
+
 }
